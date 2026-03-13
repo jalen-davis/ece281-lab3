c9cad1d6aa7c14b3c1f25549cef37d1f669ad84e
JalenDavis05 <226013171+jalen-davis@users.noreply.github.com>
2026-03-13 13:51:22 -0600

    testbench is passing

--- src/thunderbird_fsm.vhd
+++ src/thunderbird_fsm.vhd
@@ -105,18 +105,18 @@ begin
 	-- CONCURRENT STATEMENTS --------------------------------------------------------	
 	-- next state logic
 	f_Q_next(0) <= (not f_Q(0) and not f_Q(2) and i_left) or (not f_Q(0) and f_Q(1));
-	f_Q_next(1) <= (not f_Q(0) and not f_Q(1) and not f_Q(2) and not i_left and i_right) or (not f_Q(0) and f_Q(1) and not f_Q(2)) or (f_Q(0) and not f_Q(1) and f_Q(2));
-	f_Q_next(2) <= (not f_Q(0) and not f_Q(1) and not f_Q(2) and i_left and not i_right) or (f_Q(0) and f_Q(1) and not f_Q(2)) or (f_Q(0) and not f_Q(1) and f_Q(2));
+	f_Q_next(1) <= (not f_Q(0) and f_Q(1)) or (f_Q(0) and not f_Q(1) and f_Q(2)) or (not f_Q(0) and not f_Q(2) and not i_left and i_right);
+	f_Q_next(2) <= (not f_Q(0) and not f_Q(1) and not f_Q(2) and i_left and not i_right) or (f_Q(0) and f_Q(1) and not f_Q(2)) or (f_Q(0) and not f_Q(1) and f_Q(2)) or (not f_Q(0) and f_Q(1) and f_Q(2));
 	
 	-- output logic
 	-- RA/LA = lsb and RC/LC = msb
-	o_lights_L(2) <= (f_Q(0) and not f_Q(1) and not f_Q(2)) or (not f_Q(0) and not f_Q(1) and not f_Q(2));
+	o_lights_L(2) <= (f_Q(0) and not f_Q(1) and not f_Q(2)) or (f_Q(0) and f_Q(1) and f_Q(2));
 	o_lights_L(1) <= (f_Q(0) and not f_Q(1) and not f_Q(2)) or (not f_Q(0) and f_Q(1) and f_Q(2)) or (f_Q(0) and f_Q(1) and f_Q(2));
-	o_lights_L(0) <= (f_Q(0) and not f_Q(1) and f_Q(2)) or (f_Q(0) and not f_Q(1) and f_Q(2)) or (not f_Q(0) and f_Q(1) and f_Q(2)) or (f_Q(0) and f_Q(1) and f_Q(2));
+	o_lights_L(0) <= (f_Q(0) and not f_Q(1) and not f_Q(2)) or (f_Q(0) and not f_Q(1) and f_Q(2)) or (not f_Q(0) and f_Q(1) and f_Q(2)) or (f_Q(0) and f_Q(1) and f_Q(2));
     
-	o_lights_R(2) <= (f_Q(0) and not f_Q(1) and f_Q(2)) or (not f_Q(0) and not f_Q(1) and f_Q(2));
-	o_lights_R(1) <= (f_Q(0) and not f_Q(1) and f_Q(2)) or (f_Q(0) and f_Q(1) and not f_Q(2)) or (not f_Q(0) and not f_Q(1) and f_Q(2));
-	o_lights_R(0) <= (f_Q(0) and not f_Q(1) and f_Q(2)) or (not f_Q(0) and f_Q(1) and not f_Q(2)) or (f_Q(0) and f_Q(1) and not f_Q(2)) or (not f_Q(0) and not f_Q(1) and f_Q(2));
+	o_lights_R(2) <= (f_Q(0) and not f_Q(1) and not f_Q(2)) or (not f_Q(0) and not f_Q(1) and f_Q(2));
+	o_lights_R(1) <= (f_Q(0) and not f_Q(1) and not f_Q(2)) or (f_Q(0) and f_Q(1) and not f_Q(2)) or (not f_Q(0) and not f_Q(1) and f_Q(2));
+	o_lights_R(0) <= (f_Q(0) and not f_Q(1) and not f_Q(2)) or (not f_Q(0) and f_Q(1) and not f_Q(2)) or (f_Q(0) and f_Q(1) and not f_Q(2)) or (not f_Q(0) and not f_Q(1) and f_Q(2));
     ---------------------------------------------------------------------------------
 	
 	-- PROCESSES --------------------------------------------------------------------
--- src/thunderbird_fsm_tb.vhd
+++ src/thunderbird_fsm_tb.vhd
@@ -57,28 +57,100 @@ end thunderbird_fsm_tb;
 architecture test_bench of thunderbird_fsm_tb is 
 	
 	component thunderbird_fsm is 
---	  port(
-		
---	  );
+	  port(
+        i_clk, i_reset  : in    std_logic;
+        i_left, i_right : in    std_logic;
+        o_lights_L      : out   std_logic_vector(2 downto 0);
+        o_lights_R      : out   std_logic_vector(2 downto 0)
+	  );
 	end component thunderbird_fsm;
 
 	-- test I/O signals
+    --create signals for each port thing in thunderbird_fsm
+    signal w_i_clk : std_logic := '0';
+    signal w_i_reset : std_logic := '0';
+    signal w_i_left : std_logic := '0';
+    signal w_i_right : std_logic := '0';
+    signal w_o_lights_L : std_logic_vector(2 downto 0);
+    signal w_o_lights_R : std_logic_vector(2 downto 0);
 	
-	-- constants
+	-- constants don't worry about this
 	
+	-- Clock period definitions 
+	constant k_clk_period : time := 10 ns;
 	
 begin
-	-- PORT MAPS ----------------------------------------
-	
+	-- PORT MAPS ---------------------------------------- for wires
+	-- Instantiate the Unit Under Test (UUT)
+   uut: thunderbird_fsm port map (
+          i_clk => w_i_clk, 
+          i_reset => w_i_reset,
+          i_left => w_i_left,
+          i_right => w_i_right,
+          o_lights_L => w_o_lights_L,
+          o_lights_R => w_o_lights_R
+        );
 	-----------------------------------------------------
 	
 	-- PROCESSES ----------------------------------------	
-    -- Clock process ------------------------------------
-    
+    -- Clock process ------------------------------------                      -- ask about this too
+	clk_proc : process
+	begin
+		w_i_clk <= '0';
+        wait for k_clk_period/2;
+		w_i_clk <= '1';
+		wait for k_clk_period/2;
+	end process;
 	-----------------------------------------------------
 	
-	-- Test Plan Process --------------------------------
+	-- Test Plan Process -------------------------------- asserts
+	sim_proc: process
+	begin
+		-- sequential timing		
+		w_i_reset <= '1';
+		wait for k_clk_period*1;
+		  assert w_o_lights_R = "000" report "bad reset" severity failure;
+		  assert w_o_lights_L = "000" report "bad reset" severity failure;
+		
+		w_i_reset <= '0';
+		wait for k_clk_period*1;
+	   
+		w_i_left <= '1'; w_i_right <= '0';
+		wait for k_clk_period;
+		  assert w_o_lights_L = "001" report "bad on first left light" severity failure;
+		  
+		--w_i_left <= '1'; w_i_right <= '0';
+		wait for k_clk_period;
+		  assert w_o_lights_L = "011" report "bad on second left light" severity failure;
+		  
+		--w_i_left <= '1'; w_i_right <= '0';
+		wait for k_clk_period;
+		  assert w_o_lights_L = "111" report "bad on third left light" severity failure;
+		  
+		wait for k_clk_period;
+		  assert w_o_lights_L = "000" report "bad on left off" severity failure;
+		  
+		  
+		w_i_left <= '0'; w_i_right <= '1';
+		wait for k_clk_period;
+		  assert w_o_lights_R = "001" report "bad on first right light" severity failure;
+		  
+		wait for k_clk_period;
+		  assert w_o_lights_R = "011" report "bad on second right light" severity failure;
+		  
+		wait for k_clk_period;
+		  assert w_o_lights_R = "111" report "bad on third right light" severity failure;
+		
+		--w_i_reset <= '0';
+		--wait for k_clk_period*1;
+		
 	
+		--w_C <= '0'; 
+		--wait for k_clk_period;
+            --assert w_stoplight = "100" report "should be red when no car" severity failure;
+            
+		wait;
+	end process;
 	-----------------------------------------------------	
 	
 end test_bench;
